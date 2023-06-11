6a166401049ca7b7e938a3365defc154c860f88f
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-06-11 23:53:29 +0530

    Updates Footer.js

--- Venus/src/components/Footer.jsx
+++ Venus/src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
+    © 2023 Venus, Inc.
+  </div>
   )
 }
 
